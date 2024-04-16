9d883ba36c4685e0f932d935a42a541cdd5536b8
supercat0867 <howietung@163.com>
2024-04-16 21:14:26 +0800

    fix(bugs)：修复未知bug。

--- demo/demo.go
+++ demo/demo.go
@@ -17,32 +17,9 @@ type data struct {
 	Body     string `json:"body"`
 }
 
-// CorsMiddleware 跨域配置中间件
-func CorsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		method := c.Request.Method
-		origin := c.Request.Header.Get("Origin")
-
-		if origin != "" {
-			c.Header("Access-Control-Allow-Origin", origin)
-			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE, PATCH")
-			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,X-Refresh-Token")
-			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
-			c.Header("Access-Control-Allow-Credentials", "true")
-		}
-
-		if method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-		}
-		c.Next()
-	}
-}
-
 // 一个用于测试的demo，有基础的HTTP方法响应
 func main() {
 	r := gin.Default()
-
-	r.Use(CorsMiddleware())
 
 	r.GET("", func(c *gin.Context) {
 		c.JSON(http.StatusOK, Response{
